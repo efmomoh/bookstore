c432e652c6fa93e2766df75e5f55bd781eb705ab
efmomoh <efmomoh25@gmail.com>
2023-09-30 14:24:37 +0000

    Adds Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import './Navbar.css';
+// import { BiCircle } from 'react-icons/bi';
+import { BsPersonCircle } from 'react-icons/bs';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="nav">
+    <h1 className="head">Bookstore CMS</h1>
+    <ul>
+      <li>
+        <NavLink to="/">BOOKS</NavLink>
+      </li>
+      <span />
+      <li>
+        <NavLink to="/categories">CATEGORIES</NavLink>
+      </li>
+      <span />
+    </ul>
+    <div className="login">
+      <BsPersonCircle />
+    </div>
+  </nav>
+);
+
+export default Navbar;
