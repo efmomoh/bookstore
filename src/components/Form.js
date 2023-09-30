ab340ff723502a004cfa1fc3a69f82c15ed59121
efmomoh <efmomoh25@gmail.com>
2023-09-30 14:24:11 +0000

    Adds Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import './Form.css';
+
+const Form = () => (
+  <>
+    <form className="form">
+      <input type="text" placeholder="Book title" />
+      <input type="text" placeholder="Author" />
+    </form>
+  </>
+);
+
+export default Form;
