892285e76fba17a0f994557a75841e8f200f8f72
efmomoh <efmomoh25@gmail.com>
2023-09-30 22:34:11 +0000

    Adds BookForm.js

---
+++ src/components/BookForm.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const BookForm = ({ addBook }) => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+
+  const submitBookHandler = (e) => {
+    e.preventDefault();
+
+    const newTitle = e.target.title.value;
+    const newCategory = e.target.category.value;
+
+    if (newTitle && author && newCategory) {
+      const newBook = {
+        id: Date.now(),
+        title: newTitle,
+        author,
+        category: newCategory,
+      };
+
+      addBook(newBook);
+    }
+
+    setTitle('');
+    setAuthor('');
+    setCategory('');
+  };
+
+  return (
+    <div className="add-book-div">
+      <h1 className="add-book">Add New Book</h1>
+      <form onSubmit={submitBookHandler}>
+        <div className="form-container">
+          <div>
+            <label htmlFor="title">
+              <input
+                type="text"
+                id="title"
+                placeholder="Book title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="author">
+              <input
+                type="text"
+                id="author"
+                placeholder="Book author"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="category">
+              <select
+                id="category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                required
+              >
+                <option value="Category"> Category</option>
+                <option value="Action">Action</option>
+                <option value="Science Fiction">Science Fiction</option>
+                <option value="Economy">Economy</option>
+                <option value="Fiction">Fiction</option>
+                <option value="NonFiction">Nonfiction</option>
+              </select>
+            </label>
+          </div>
+          <div>
+            <button className="form-btn" type="submit">
+              Add
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+BookForm.propTypes = {
+  addBook: PropTypes.func.isRequired,
+};
+
+export default BookForm;
