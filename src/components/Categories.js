a482bb2960cbefbdd20e3a79f1fbaa556e5f0238
efmomoh <efmomoh25@gmail.com>
2023-09-30 14:23:12 +0000

    Adds Categories.js

---
+++ src/components/Categories.js
@@ -0,0 +1,13 @@
+import './Categories.css';
+
+const Categories = () => (
+  <>
+    <div className="btn-container">
+      <button type="button" className="btn-status">
+        Check status
+      </button>
+    </div>
+  </>
+);
+
+export default Categories;
