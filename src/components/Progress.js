f3998925e99f307a35af5217faf5aad3f4d3213a
efmomoh <efmomoh25@gmail.com>
2023-10-07 13:55:51 +0000

    Adds Progress.js

---
+++ src/components/Progress.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Styles/Progress.css';
+
+const Progress = () => (
+  <div className="progress-container">
+    <div className="oval" />
+    <div className="progress">
+      <h2>85%</h2>
+      <p>completed</p>
+    </div>
+  </div>
+);
+
+export default Progress;
